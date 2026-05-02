323ddadf02b5fb62409e134be1d198ba95b763b5
Prakruthi U <uprakruthi77@gmail.com>
2026-05-02 10:20:22 +0530

    Update app.py

--- urban-crime-heatmap/app.py
+++ urban-crime-heatmap/app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     page_title="CrimeScope | Urban Crime Intelligence",
     page_icon="🔴",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="expanded",  # ← always starts open
 )
 
 from utils.theme import GLOBAL_CSS
